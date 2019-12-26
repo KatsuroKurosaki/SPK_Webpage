7f16547888bcc4e9236e99bf0d21222dfafb8eb7
Joaquim Sardiña <joaquimsb89@gmail.com>
2019-12-26 12:06:18 +0100

    Code formatting

--- class/Permission.php
+++ class/Permission.php
@@ -1,11 +1,12 @@
 <?php
-class Permission {
+class Permission
+{
 	// Nada
 	const	SIN_ACCESO					=   0;
-	
+
 	// Constantes para la web normal
-	
-	
+
+
 	// Constantes para Web staff
 	const	STAFF_WEBCONSOLE			=    1;
 	const	STAFF_BANLIST 				=    2;
@@ -17,10 +18,10 @@ class Permission {
 	const	STAFF_WEBCONSOLEACCEPTCMDS	=  128;
 	const	STAFF_RANGOSEXTRACMDS		=  256;
 	const	STAFF_PERMISSIONSEX			=  512;
-	
-	
-	public static function checkPermission($toCheck,$perm){
-		return(boolean) (($toCheck & $perm)==$perm);
+
+
+	public static function checkPermission($toCheck, $perm)
+	{
+		return (bool) (($toCheck & $perm) == $perm);
 	}
 }
-?>
--- class/PermissionsEX.php
+++ class/PermissionsEX.php
@@ -1,31 +1,42 @@
 <?php
-class PermissionsEX {
-	public static function setRank($conn,$uuid,$playername,$rank) {
+class PermissionsEX
+{
+	public static function setRank($conn, $uuid, $playername, $rank)
+	{
 		$sql = "INSERT INTO pex_entity (`name`,`type`,`default`) VALUES (?,1,0);";
 		$stmt = $conn->prepare($sql);
-		if($stmt===false){ die( $conn->error ); }
-		$stmt->bind_param( 's',
+		if ($stmt === false) {
+			die($conn->error);
+		}
+		$stmt->bind_param(
+			's',
 			$uuid
 		);
 		$stmt->execute();
 		$stmt->close();
-			
+
 		$sql = "INSERT INTO pex_inheritance (`child`,`parent`, `type` ) VALUES (?,?,1);";
 		$stmt = $conn->prepare($sql);
-		if($stmt===false){ die( $conn->error ); }
-		$stmt->bind_param( 'ss',
+		if ($stmt === false) {
+			die($conn->error);
+		}
+		$stmt->bind_param(
+			'ss',
 			$uuid,
 			$rank
 		);
 		$stmt->execute();
 		$stmt->close();
-		
+
 		$mc_modes = PermissionsEX::getMcModes($conn);
-		foreach($mc_modes as $k=>$v){
-			$sql = "INSERT INTO pex_permissions_".$v['dirname']." (`name`,`type`,`permission`,`world`,`value`) VALUES (?,1,'name','',?);";
+		foreach ($mc_modes as $k => $v) {
+			$sql = "INSERT INTO pex_permissions_" . $v['dirname'] . " (`name`,`type`,`permission`,`world`,`value`) VALUES (?,1,'name','',?);";
 			$stmt = $conn->prepare($sql);
-			if($stmt===false){ die( $conn->error ); }
-			$stmt->bind_param( 'ss',
+			if ($stmt === false) {
+				die($conn->error);
+			}
+			$stmt->bind_param(
+				'ss',
 				$uuid,
 				$playername
 			);
@@ -33,50 +44,80 @@ class PermissionsEX {
 			$stmt->close();
 		}
 	}
-	
-	public static function delRank($conn,$uuid){
+
+	public static function delRank($conn, $uuid)
+	{
 		$sql = "DELETE FROM pex_entity WHERE `name` = ?;";
 		$stmt = $conn->prepare($sql);
-		if($stmt === false){$out['status']="ko"; $out['msg']=$conn->error; die(json_encode($out));}
-		$stmt->bind_param( 's',
+		if ($stmt === false) {
+			$out['status'] = "ko";
+			$out['msg'] = $conn->error;
+			die(json_encode($out));
+		}
+		$stmt->bind_param(
+			's',
 			$uuid
 		);
 		$stmt->execute();
-		if($stmt->error){$out['status']="ko"; $out['msg']=$stmt->error; die(json_encode($out));}
+		if ($stmt->error) {
+			$out['status'] = "ko";
+			$out['msg'] = $stmt->error;
+			die(json_encode($out));
+		}
 		$stmt->close();
 
 		$sql = "DELETE FROM pex_inheritance WHERE `child` = ?;";
 		$stmt = $conn->prepare($sql);
-		if($stmt === false){$out['status']="ko"; $out['msg']=$conn->error; die(json_encode($out));}
-		$stmt->bind_param( 's',
+		if ($stmt === false) {
+			$out['status'] = "ko";
+			$out['msg'] = $conn->error;
+			die(json_encode($out));
+		}
+		$stmt->bind_param(
+			's',
 			$uuid
 		);
 		$stmt->execute();
-		if($stmt->error){$out['status']="ko"; $out['msg']=$stmt->error; die(json_encode($out));}
+		if ($stmt->error) {
+			$out['status'] = "ko";
+			$out['msg'] = $stmt->error;
+			die(json_encode($out));
+		}
 		$stmt->close();
 
 		$mc_modes = PermissionsEX::getMcModes($conn);
-		foreach($mc_modes as $k=>$v){
-			$sql = "DELETE FROM pex_permissions_".$v['dirname']." WHERE `name` = ?;";
+		foreach ($mc_modes as $k => $v) {
+			$sql = "DELETE FROM pex_permissions_" . $v['dirname'] . " WHERE `name` = ?;";
 			$stmt = $conn->prepare($sql);
-			if($stmt === false){$out['status']="ko"; $out['msg']=$conn->error; die(json_encode($out));}
-			$stmt->bind_param( 's',
+			if ($stmt === false) {
+				$out['status'] = "ko";
+				$out['msg'] = $conn->error;
+				die(json_encode($out));
+			}
+			$stmt->bind_param(
+				's',
 				$uuid
 			);
 			$stmt->execute();
-			if($stmt->error){$out['status']="ko"; $out['msg']=$stmt->error; die(json_encode($out));}
+			if ($stmt->error) {
+				$out['status'] = "ko";
+				$out['msg'] = $stmt->error;
+				die(json_encode($out));
+			}
 			$stmt->close();
 		}
 	}
-	
-	public static function getMcModes($conn){
+
+	public static function getMcModes($conn)
+	{
 		$sql = "SELECT id, modename, dirname FROM mc_modes WHERE use_pex;";
 		$stmt = $conn->prepare($sql);
-		if($stmt===false){ die( $conn->error ); }
+		if ($stmt === false) {
+			die($conn->error);
+		}
 		$stmt->execute();
 		$mc_modes = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 		$stmt->close();
 		return $mc_modes;
 	}
 }
-?>
